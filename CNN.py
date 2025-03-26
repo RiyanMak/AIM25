3685a34f52d4efc13343dea9fb10f891dafe35cc
Riyan Maknojia <riyanmak123@gmail.com>
2025-03-26 10:31:10 -0500

    Cherry picked CNN changes

--- CNN.py
+++ CNN.py
@@ -10,12 +10,11 @@ class ConvNeuralNet(nn.module):
 
         self.conv1 = nn.conv2d(in_channels = in_channels, out_channels = 8, kernel_size=3)
         self.conv2 = nn.conv2d(in_channels = 8, out_channels = 16, kernel_size=3)
-        self.conv3 = nn.conv2d(in_channels = 16, out_channels = 32, kernel_size=3)
         self.maxPool1 = nn.MaxPool2d(kernel_size=2,stride=2)
+
+        self.conv3 = nn.conv2d(in_channels = 16, out_channels = 32, kernel_size=3)
+        self.conv4 = nn.conv2d(in_channels = 32, out_channels = 64, kernel_size=3)
+        self.maxPool2 = nn.MaxPool2d(kernel_size=2,stride=2) 
+
     
     #def forward_prop(self, x):
-
-        
-
-
-
