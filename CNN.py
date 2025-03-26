ab310ae763c6dcae385b526e07cb52f40a5dcf6a
Riyan Maknojia <riyanmak123@gmail.com>
2025-03-26 00:24:30 -0500

    var name changed

--- CNN.py
+++ CNN.py
@@ -14,7 +14,7 @@ class ConvNeuralNet(nn.module):
 
         self.conv3 = nn.conv2d(in_channels = 16, out_channels = 32, kernel_size=3)
         self.conv4 = nn.conv2d(in_channels = 32, out_channels = 64, kernel_size=3)
-        self.maxPool1 = nn.MaxPool2d(kernel_size=2,stride=2) 
+        self.maxPool2 = nn.MaxPool2d(kernel_size=2,stride=2) 
 
     
     #def forward_prop(self, x):
